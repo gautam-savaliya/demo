589e10e8fec347e38af2c4ef55fb31acb5f960cb
gautam-savaliya <gautams@itpathsolutions.com>
2022-07-07 18:23:28 +0530

    changesss

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import  express  from 'express';
+const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 4000;
 import db from './models';
@@ -20,7 +21,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors())
 
-app.use(function (req:any, res:any, next) {
+app.use(function (req:any, res:any, next:any) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
